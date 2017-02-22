9cb1b21eb1b9e3ab96d34833f7fa403d983a5c2f
Jackey Cheung <eidng8@users.noreply.github.com>
2017-02-22 12:18:19 +0800

    use `@HostBinding`

--- src/directives/collapsible/collapsible.directive.ts
+++ src/directives/collapsible/collapsible.directive.ts
@@ -4,51 +4,97 @@
  *  @link    https://github.com/eidng8/stthb
  */
 
-import { Directive, ElementRef, Renderer, OnInit } from '@angular/core';
+import find from 'lodash-es/find';
+import sumBy from 'lodash-es/sumBy';
+import {
+  Directive,
+  ElementRef,
+  AfterViewInit,
+  AfterViewChecked,
+  HostBinding,
+  Renderer
+} from '@angular/core';
 
 @Directive(
   {
-    // selectors like `'[collapsible] ion-list-header'` doesn't seem to
-    // work. Does Angular use single level selector?
-    // seems we have to use ngOnInit for this purpose.
-    selector: '[hbCollapsible]',
+    selector: '[collapsible]',
   })
-export class CollapsibleDirective implements OnInit {
+export class CollapsibleDirective implements AfterViewInit, AfterViewChecked
+{
   static readonly cssClass: string = 'collapsed';
 
-  private elem: ElementRef;
-  private renderer: Renderer;
+  @HostBinding(`class.${CollapsibleDirective.cssClass}`)
+  private _collapsed: boolean = true;
 
-  public constructor(el: ElementRef, renderer: Renderer) {
-    this.elem = el;
-    this.renderer = renderer;
+  constructor(private _elem: ElementRef, private _renderer: Renderer)
+  {
   }
 
-  public ngOnInit(): any {
+  ngAfterViewChecked(): void
+  {
+    // can't use host binding here, otherwise angular will throw
+    // EXCEPTION: Expression has changed after it was checked.
+    this._elem.nativeElement.style.maxHeight = this.calcHeight() + 'px';
+  }
+
+  ngAfterViewInit(): void
+  {
     // we are actually handling clicks to the header of the list,
     // instead of the list itself.
     // just to remember, event registration can't be done in constructor,
     // due to the fact that the HTML template is not processed yet.
-    let header: HTMLElement =
-      this.elem.nativeElement.getElementsByTagName('ion-list-header')[0];
-    if(header) {
-      this.renderer.listen(header, 'click', () => {
-        if(this.elem.nativeElement.classList.contains(
-            CollapsibleDirective.cssClass)) {
-          this.expend();
-        }
-        else {
-          this.collapse();
-        }
-      });
+    this.attachEvent(this.getHeader());
+  }
+
+  toggle(): void
+  {
+    this._collapsed = !this._collapsed;
+  }
+
+  collapse(): void
+  {
+    this._collapsed = true;
+  }
+
+  expend(): void
+  {
+    this._collapsed = false;
+  }
+
+  /**
+   * Attach the `click` event handler to list header
+   * @param header
+   */
+  private attachEvent(header: HTMLElement): void
+  {
+    if(!header) {
+      return;
     }
+
+    this._renderer.listen(header, 'click', () => this.toggle());
   }
 
-  private collapse(): void {
-    this.elem.nativeElement.classList.add(CollapsibleDirective.cssClass);
+  /**
+   * Get the list header
+   */
+  private getHeader(): HTMLElement
+  {
+    return find(
+      this.getDirectChildren(),
+      (elem: HTMLElement) => 'ion-list-header' == elem.tagName.toLowerCase());
   }
 
-  private expend(): void {
-    this.elem.nativeElement.classList.remove(CollapsibleDirective.cssClass);
+  /**
+   * Can't find a way to get direct children without using `navtiveElement`
+   */
+  private getDirectChildren(): HTMLElement[]
+  {
+    return this._elem.nativeElement.children
+      ? this._elem.nativeElement.children : [];
+  }
+
+  private calcHeight(): number
+  {
+    return sumBy(this.getDirectChildren(), elem => elem.offsetHeight);
   }
 }

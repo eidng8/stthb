2d629d6a66c97e415ef2ed290fa2c22dbee847eb
eidng8 <cheung.jackey@gmail.com>
2017-02-23 22:24:26 +0800

    add test grouping

--- src/pages/home/home.page.spec.ts
+++ src/pages/home/home.page.spec.ts
@@ -9,36 +9,36 @@ import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePageComponent } from './home.page';
 
-describe('Home Page:', () => {
+describe('Pages:', () => {
+  describe('Home Page', () => {
 
-  let comp: HomePageComponent;
-  let fixture: ComponentFixture<HomePageComponent>;
-  let de: DebugElement;
+    let comp: HomePageComponent;
+    let fixture: ComponentFixture<HomePageComponent>;
+    let de: DebugElement;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule(
-      {
-        declarations: [HomePageComponent],
-        providers:    [{provide: NavController, useValue: NavController}],
-        schemas:      [CUSTOM_ELEMENTS_SCHEMA],
-      });
-    fixture = TestBed.createComponent(HomePageComponent);
-    // #trick
-    // if you want to trigger ionViewWillEnter automatically de-comment the
-    // following line
-    // fixture.componentInstance.ionViewWillEnter();
-    fixture.detectChanges();
-    comp = fixture.componentInstance;
-    de = fixture.debugElement;
-  });
+    beforeEach(() => {
+      TestBed.configureTestingModule(
+        {
+          declarations: [HomePageComponent],
+          providers:    [{provide: NavController, useValue: NavController}],
+          schemas:      [CUSTOM_ELEMENTS_SCHEMA],
+        });
+      fixture = TestBed.createComponent(HomePageComponent);
+      // #trick
+      // if you want to trigger ionViewWillEnter automatically de-comment the
+      // following line
+      // fixture.componentInstance.ionViewWillEnter();
+      fixture.detectChanges();
+      comp = fixture.componentInstance;
+      de = fixture.debugElement;
+    });
 
-  describe('.constructor()', () => {
-    it('Should be defined', () => {
+    it('can be created', () => {
       expect(comp).toBeDefined();
     });
-  });
 
-  it('Should has welcome message', () => {
-    expect(comp.message).toEqual('Welcome');
+    it('should have welcome message', () => {
+      expect(comp.message).toEqual('Welcome');
+    });
   });
 });

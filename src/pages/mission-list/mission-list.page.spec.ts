5aadf405b22808d8d23890df54574ce9f0d0b49d
eidng8 <cheung.jackey@gmail.com>
2017-02-23 22:28:55 +0800

    add test grouping

--- src/pages/mission-list/mission-list.page.spec.ts
+++ src/pages/mission-list/mission-list.page.spec.ts
@@ -8,36 +8,36 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { MissionListPageComponent } from './mission-list.page';
 
-describe('Mission List Page:', () => {
+describe('Pages:', () => {
+  describe('Mission List', () => {
 
-  let comp: MissionListPageComponent;
-  let fixture: ComponentFixture<MissionListPageComponent>;
-  let de: DebugElement;
+    let comp: MissionListPageComponent;
+    let fixture: ComponentFixture<MissionListPageComponent>;
+    let de: DebugElement;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule(
-      {
-        declarations: [MissionListPageComponent],
-        // providers:    [{provide: NavController, useValue: NavController}],
-        schemas:      [CUSTOM_ELEMENTS_SCHEMA],
-      });
-    fixture = TestBed.createComponent(MissionListPageComponent);
-    // #trick
-    // if you want to trigger ionViewWillEnter automatically de-comment the
-    // following line
-    // fixture.componentInstance.ionViewWillEnter();
-    fixture.detectChanges();
-    comp = fixture.componentInstance;
-    de = fixture.debugElement;
-  });
+    beforeEach(() => {
+      TestBed.configureTestingModule(
+        {
+          declarations: [MissionListPageComponent],
+          // providers:    [{provide: NavController, useValue: NavController}],
+          schemas:      [CUSTOM_ELEMENTS_SCHEMA],
+        });
+      fixture = TestBed.createComponent(MissionListPageComponent);
+      // #trick
+      // if you want to trigger ionViewWillEnter automatically de-comment the
+      // following line
+      // fixture.componentInstance.ionViewWillEnter();
+      fixture.detectChanges();
+      comp = fixture.componentInstance;
+      de = fixture.debugElement;
+    });
 
-  describe('.constructor()', () => {
     it('Should be defined', () => {
       expect(comp).toBeDefined();
     });
-  });
 
-  // it('Should has welcome message', () => {
-  //   expect(comp.message).toEqual('Welcome');
-  // });
+    // it('Should has welcome message', () => {
+    //   expect(comp.message).toEqual('Welcome');
+    // });
+  });
 });

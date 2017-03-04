ce1745c24b62eba9ca666f94e399fffc254f5290
eidng8 <cheung.jackey@gmail.com>
2017-03-04 18:11:07 +0800

    rename `sh` to `scripts`, as it contains more than just bash scripts

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -1,3 +1,9 @@
+/*
+ *  @author  eidng8
+ *  @license https://creativecommons.org/licenses/by-sa/4.0/
+ *  @link    https://github.com/eidng8/stthb
+ */
+
 'use strict';
 
 startWebServer('127.0.0.1', 'www', startTest);

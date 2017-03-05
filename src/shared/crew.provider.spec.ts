5623927cfe022e59d318c793ed32cab4ad13325f
eidng8 <cheung.jackey@gmail.com>
2017-03-05 22:40:13 +0800

    use model factory

---
+++ src/shared/crew.provider.spec.ts
@@ -0,0 +1,34 @@
+/*
+ *  @author  eidng8
+ *  @license https://creativecommons.org/licenses/by-sa/4.0/
+ *  @link    https://github.com/eidng8/stthb
+ */
+
+import { TestBed, inject } from '@angular/core/testing';
+import { provideMockDataService } from '../testing/data.service.mock';
+import { Factory } from './factory';
+import { CrewProvider } from './crew.provider';
+import { DataService } from './data.service';
+
+describe('Crew Provider:', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule(
+      {
+        providers: [
+          provideMockDataService,
+          Factory,
+          CrewProvider,
+        ],
+      });
+  });
+
+  it('can be injected', inject([CrewProvider], (crew: CrewProvider) => {
+    expect(crew).toBeDefined();
+  }));
+
+  it('should fetch & store local data', inject([CrewProvider, DataService],
+    (crew: CrewProvider, server: DataService) => {
+      expect(crew.all.length).toBe(server.crew.length);
+    }));
+});
--- src/shared/crew.provider.ts
+++ src/shared/crew.provider.ts
@@ -5,48 +5,39 @@
  */
 
 import each from 'lodash-es/each';
-import forOwn from 'lodash-es/forOwn';
+import { Injectable } from '@angular/core';
+import { Factory } from './factory';
 import { DataService } from './data.service';
 import { MemberModel } from '../models/member.model';
 import { IMember } from '../interfaces/member.interface';
-import { TSkills, SkillsModel } from '../models/skills.model';
 
+@Injectable()
 /**
  * Fundamental crew data processing features
  */
 export class CrewProvider {
 
-  constructor(private _data: DataService) {
+  protected crew: MemberModel[];
+
+  constructor(private _factory: Factory, server: DataService) {
+    this.load(server);
   }
 
-  each(func: (member: MemberModel, index: number) => void): void {
-    each(this._data.crew, (data: IMember, idx: number) => {
-      func(this.model(data), idx);
+  /**
+   * Returns all crew members
+   */
+  get all(): MemberModel[] {
+    return this.crew;
+  }
+
+  /**
+   * Load crew member from the given server data
+   */
+  load(server: DataService): void {
+    this.crew = [];
+    each(server.crew, (data: IMember, idx: number) => {
+      this.crew[idx] = this._factory.member(data);
+      /* TODO build various indices here */
     });
   }
-
-  private model(data: IMember): MemberModel {
-    const model: MemberModel = new MemberModel();
-    model.character = this._data.characters[data.character];
-    model.name = data.name;
-    model.picture = data.picture;
-    model.race = this._data.races[data.race];
-    model.skills = this.loadSkills(data.skills);
-    model.stars = data.stars;
-    model.traits = data.traits.map(trait => this._data.traits[trait]);
-    return model;
-  }
-
-  private loadSkills(skills: TSkills): SkillsModel {
-    const model: SkillsModel = new SkillsModel();
-    forOwn(skills, (values, key) => {
-      if(!Array.isArray(values) || values.length != 3) {
-        console.warn('Invalid skill values', values, `for key ${key}`);
-      } else {
-        model[this._data.skills[key]] = values;
-      }
-    });
-    return model;
-  }
-
 }

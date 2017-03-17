1da431f1e5ea6cf408563171a906cee962fd4ef0
eidng8 <cheung.jackey@gmail.com>
2017-03-18 00:09:39 +0800

    test crew mission association

--- src/providers/missions.provider.spec.ts
+++ src/providers/missions.provider.spec.ts
@@ -9,16 +9,12 @@ import { MissionsProvider } from './missions.provider';
 import { IServerData } from '../interfaces/server-data.interface';
 import { MemberModel } from '../models/member.model';
 
+describe('Providers:', () => {
 
-describe('Providers:', () =>
-{
-
-  describe('Missions Provider:', () =>
-  {
+  describe('Missions Provider:', () => {
     let data: IServerData, crew: MemberModel[];
 
-    beforeAll(() =>
-    {
+    beforeAll(() => {
       data = require('../../www/mission-test.data.json');  // tslint:disable-line
       crew = data.crew.map(member => {
         const mem: MemberModel = new MemberModel();
@@ -27,47 +23,42 @@ describe('Providers:', () =>
       });
     }); // end beforeAll()
 
-    beforeEach(() =>
-    {
+    beforeEach(() => {
       TestBed.configureTestingModule({providers: [MissionsProvider]});
     });
 
     it('can be injected', inject([MissionsProvider],
-      (missions: MissionsProvider) =>
-      {
+      (missions: MissionsProvider) => {
         expect(missions).toBeDefined();
         expect(missions).not.toBeNull();
       }));
 
     it('should load from server data',
-      inject([MissionsProvider], (missions: MissionsProvider) =>
-        {
-          missions.load(data);
-          expect(missions.all.length).toBe(data.missions.length);
-        }));
+      inject([MissionsProvider], (missions: MissionsProvider) => {
+        missions.load(data);
+        expect(missions.all.length).toBe(data.missions.length);
+      }));
 
     it('should properly associate mission steps and crew',
-      inject([MissionsProvider], (missions: MissionsProvider) =>
-        {
-          missions.load(data);
-          missions.loadCrew(crew);
+      inject([MissionsProvider], (missions: MissionsProvider) => {
+        missions.load(data);
+        missions.loadCrew(crew);
 
-          // check association from mission side
-          expect(missions.all[0].steps[0].crew.critical[0].name)
-            .toBe('1701 Sisko');
-          // the above one is easier to see in karma output
-          // below one is the real deal
-          expect(missions.all[0].steps[0].crew.critical[0])
-            .toBe(crew[data.missions[0].steps[0].crew.critical[0]]);
+        // check association from mission side
+        expect(missions.all[0].steps[0].crew.critical[0].name)
+          .toBe('1701 Sisko');
+        // the above one is easier to see in karma output
+        // below one is the real deal
+        expect(missions.all[0].steps[0].crew.critical[0])
+          .toBe(data.missions[0].steps[0].crew.critical[0]);
 
-          // check association from crew side
-          expect(crew[3].missions.critical[0].mission.name)
-            .toBe('The Wrong Crowd');
-          expect(crew[3].missions.critical[0].mission).toBe(missions.all[0]);
-          expect(crew[3].missions.critical[0].steps).toEqual([0, 1, 2, 3]);
-        }));
+        // check association from crew side
+        expect(crew[3].missions.critical[0].mission.name)
+          .toBe('The Wrong Crowd');
+        expect(crew[3].missions.critical[0].mission).toBe(missions.all[0]);
+        expect(crew[3].missions.critical[0].steps).toEqual([0, 1, 2, 3]);
+      }));
 
   }); // end Missions Provider
 
 }); // end Providers
-
